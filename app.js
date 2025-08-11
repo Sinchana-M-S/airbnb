fefaf26b9fb99d9c2f75dff2fe8856aca95da58b
Sinchana-M-S <sinchanams932@gmail.com>
2025-08-11 23:01:11 +0530

    airbnb level2 done

--- app.js
+++ app.js
@@ -5,24 +5,23 @@ const path = require('path');
 const express = require('express');
 
 //Local Module
-const userRouter = require("./routes/userRouter")
-const {hostRouter} = require("./routes/hostRouter")
+const storeRouter = require("./routes/storeRouter")
+const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
+const errorsController = require('./controllers/errors')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded());
-app.use(userRouter);
+app.use(express.urlencoded());  //parsing req body
+app.use(storeRouter);
 app.use("/host", hostRouter);
 
-app.use(express.static(path.join(rootDir, 'public')))
+app.use(express.static(path.join(rootDir, 'public')))  //css
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found', currentPage: '404'});
-})
+app.use(errorsController.pageNotFound); //404
 
 const PORT = 3000;
 app.listen(PORT, () => {
